8602b6b1547105602c5fccff2227c74928a3d340
Jessica Stephens <jeslstep1@gmail.com>
2019-01-13 17:50:43 -0600

    ready for heroku

---
+++ build/precache-manifest.8ca6f1fee44c4034955a2c6436503006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7106fb80a41bc56813ce",
+    "url": "/static/js/main.7106fb80.chunk.js"
+  },
+  {
+    "revision": "36492c2e78f237aa83f2",
+    "url": "/static/js/1.36492c2e.chunk.js"
+  },
+  {
+    "revision": "7106fb80a41bc56813ce",
+    "url": "/static/css/main.d34e446e.chunk.css"
+  },
+  {
+    "revision": "f2fbf4baec86d71bf7c1832136b77711",
+    "url": "/index.html"
+  }
+];
